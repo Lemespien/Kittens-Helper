b312232e774fb033f9034d51fa4bbd603bedcfc5
Lemespien <Nilhogg@gmail.com>
2020-07-20 02:29:27 +0200

    changed Resources to store elements by name as key, changed how we get trade checkboxes

--- kittens.js
+++ kittens.js
@@ -1,35 +1,6 @@
 const huntAll = document.querySelector("#fastHuntContainerCount");
 const praise = document.querySelector('a[data-reactid=".0.3.0"]');
 
-
-
-/* Trading */
-// let tradingArray = Array.from(document.querySelectorAll(".title"));
-
-// let myFilteredArray = tradingArray.filter((element, index) => {
-//     if (element.innerText.search("Zebra") >= 0) {
-//         return { index, element };
-//     }
-// })
-
-
-// Can grab .craft class for these to automate it
-//let craftingArr = Array.from(document.querySelectorAll(".craft")) -> nodeList - converted to array.
-// .querySelector(".resource-name").innerText should return resource name
-// on each of them .querySelector(".all"); -> this should return the "all" link element
-
-/* Resources */
-// const woodAll = document.querySelector('div[data-reactid=".0.5.1.0.0.6.0"]');
-// const beamAll = document.querySelector('div[data-reactid=".0.5.1.0.2.6.0"]');
-// const slabAll = document.querySelector('div[data-reactid=".0.5.1.0.3.6.0"]');
-// const plateAll = document.querySelector('div[data-reactid=".0.5.1.0.4.6.0"]');
-// const steelAll = document.querySelector('div[data-reactid=".0.5.1.0.5.6.0"]');
-// const alloyAll = document.querySelector('[data-reactid=".0.5.1.0.8.6.0"]');
-// const keroseneAll = document.querySelector('[data-reactid=".0.5.1.0.d.6.0"]');
-// const parchmentAll = document.querySelector('div[data-reactid=".0.5.1.0.e.6.0"]');
-// const manuscriptAll = document.querySelector('div[data-reactid=".0.5.1.0.f.6.0"]');
-// const compendiumAll = document.querySelector('div[data-reactid=".0.5.1.0.g.6.0"]');
-
 const cultureResource = document.querySelector('div[data-reactid=".0.0.1.1.d.2"]');
 const catPower = document.querySelector('[data-reactid=".0.0.1.1.b.2"]');
 const catPowerMax = document.querySelector('[data-reactid=".0.0.1.1.b.3"]');
@@ -44,41 +15,52 @@ class CraftingResource {
     constructor(name, element) {
         this.name = name;
         this.element = element;
-        Resources.set(this, false);
+        this.automate = false;
+        Resources.set(name, this);
+    }
+
+    flipAutomate() {
+        this.automate = !this.automate;
     }
 }
 
 document.querySelector(".Trade").click();
-// this returns a list of all trade buttons + send explorers
-let tradingArray = Array.from(document.querySelectorAll(".btnContent")).filter((element, index) => {
-    if (index % 2 == 0) {
-        console.log(element);
-        return element;
-    }
-});
 
-tradingArray.forEach((element, index) => {
-    if (index == tradingArray.length - 1) {
-        // if explorers -> it's only got 1 button
-        new CraftingResource("Explorers", element.children[0]);
-        return;
-    }
+new CraftingResource("Explorers", document.querySelector(".explore .btnTitle"));
 
-    let traderName = tradingArray[index].parentElement.parentElement.parentElement.parentElement.children[1].innerText.split(" ")[0]
-    new CraftingResource(traderName, element.children[1]);
+let tradingArray = Array.from(document.querySelectorAll(".panelContainer"));
+
+tradingArray.forEach((element) => {
+    let traderName = element.querySelector(".title").innerText.split(" ")[0]; // "Lizard Friendly".split(" ")
+    let traderAll = element.querySelector(".btnContent a");
+
+    new CraftingResource(traderName, traderAll);
 });
 
 let craftingArray = Array.from(document.querySelectorAll(".craft"));
 
 craftingArray.forEach((element, index) => {
-    new CraftingResource(element.querySelector(".resource-name").innerText, element.querySelector(".all"));
+    const resourceName = element.querySelector(".resource-name").innerText;
+    const resourceElement = element.querySelector(".all");
+
+    new CraftingResource(resourceName, resourceElement);
 });
 
 const Praise = new CraftingResource("Praise", praise);
 
 const Hunt = new CraftingResource("Hunt", huntAll); // this has to be last, since it's an ordered list
 
-let ObjectKeys = Array.from(Resources.keys());
+let ObjectKeys = Array.from(Resources.values());
+
+let ClickyArray = ObjectKeys.map((element, index) => {
+    if (element.name == "steel") {
+        const plate = ObjectKeys[index - 1];
+        ObjectKeys[index - 1] = element;
+        ObjectKeys[index] = plate;
+    }
+    return element;
+})
+
 
 let craftingInterval;
 
@@ -87,7 +69,7 @@ function startInterval() {
     craftingInterval = setInterval(() => {
         try {
             ObjectKeys.forEach((key) => {
-                if (Resources.get(key) && key.element) {
+                if (key.automate && key.element) {
                     if (specialAction(key, "Hunt", catPower, catPowerMax, 0.5)) return;
                     if (specialAction(key, "Alloy", titanium, titaniumMax, 0.75)) return;
                     if (specialAction(key, "Kerosene", oil, oilMax, 0.75)) return;
@@ -171,7 +153,7 @@ function createCheckBox(resource) {
     const checkBox = document.createElement("input");
     checkBox.type = "checkBox";
     checkBox.name = resource.name;
-    checkBox.value = Resources.get(resource);
+    checkBox.value = resource.automate;
     checkBox.id = `leme_${resource.name}_id`;
 
     const checkBoxLabel = document.createElement("label");
@@ -180,7 +162,7 @@ function createCheckBox(resource) {
     checkBoxArray.push(checkBox);
     checkBoxAndLabelArray.push([checkBox, checkBoxLabel]);
     checkBox.addEventListener("click", () => {
-        Resources.set(resource, checkBox.checked);
+        resource.flipAutomate();
     });
 }
 
